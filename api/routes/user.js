a64f587b1e3918e05c7a0e4325c2b724233732e3
mesifer <brosfandy@gmail.com>
2022-04-19 19:23:55 +0700

    first

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql')
- 
+
+const  MYSQL_ADDON_HOST        = process.env.MYSQL_ADDON_HOST || "b2wnv8j339gky6irdwvb-mysql.services.clever-cloud.com"
+const  MYSQL_ADDON_PASSWORD    = process.env.MYSQL_ADDON_PASSWORD || "qWbqvd7y09lzisVBDnE5"
+const  MYSQL_ADDON_PORT        = process.env.MYSQL_ADDON_PORT || "3306"
+const  MYSQL_ADDON_USER        = process.env.MYSQL_ADDON_USER || "umvvsly1qslkep8a"
+const  MYSQL_ADDON_DB          = process.env.MYSQL_ADDON_DB || "b2wnv8j339gky6irdwvb"
+
 exports.database = mysql.createConnection({
-   host     : 'b2wnv8j339gky6irdwvb-mysql.services.clever-cloud.com',
-   user     : 'umvvsly1qslkep8a',
-   password : 'qWbqvd7y09lzisVBDnE5',
-   database : 'b2wnv8j339gky6irdwvb'
+   host     : MYSQL_ADDON_HOST,
+   user     : MYSQL_ADDON_USER,
+   password : MYSQL_ADDON_PASSWORD,
+   database : MYSQL_ADDON_DB
 });
 
 exports.register = function(req, res){
